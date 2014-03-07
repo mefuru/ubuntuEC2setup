42090c94234bf78031c5de138884bb12a9ae01c9
mefuru <mehul.mandania@gmail.com>
2014-03-07 16:06:50 +0000

    included dotfiles

--- setup.sh
+++ setup.sh
@@ -52,18 +52,20 @@ sudo apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql
 sudo mysql_install_db
 sudo /usr/bin/mysql_secure_installation
 
+# Install npm
+sudo apt-get install npm
 
-# # git pull and install dotfiles as well
-# cd $HOME
-# if [ -d ./dotfiles/ ]; then
-#     mv dotfiles dotfiles.old
-# fi
-# if [ -d .emacs.d/ ]; then
-#     mv .emacs.d .emacs.d~
-# fi
-# git clone https://github.com/startup-class/dotfiles.git
-# ln -sb dotfiles/.screenrc .
-# ln -sb dotfiles/.bash_profile .
-# ln -sb dotfiles/.bashrc .
-# ln -sb dotfiles/.bashrc_custom .
-# ln -sf dotfiles/.emacs.d .
+# git pull and install dotfiles as well
+cd $HOME
+if [ -d ./dotfiles/ ]; then
+    mv dotfiles dotfiles.old
+fi
+if [ -d .emacs.d/ ]; then
+    mv .emacs.d .emacs.d~
+fi
+git clone https://github.com/startup-class/dotfiles.git
+ln -sb dotfiles/.screenrc .
+ln -sb dotfiles/.bash_profile .
+ln -sb dotfiles/.bashrc .
+ln -sb dotfiles/.bashrc_custom .
+ln -sf dotfiles/.emacs.d .
