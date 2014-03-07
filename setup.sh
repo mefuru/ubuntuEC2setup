3a7ce11040a6515971f0fc6cff861d9ee88edaf4
mefuru <mehul.mandania@gmail.com>
2014-03-07 14:20:26 +0000

    init

---
+++ setup.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+# Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
+# for headless setup. 
+
+# Install nvm: node-version manager
+# https://github.com/creationix/nvm
+sudo apt-get install -y git
+sudo apt-get install -y curl
+curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+
+# Load nvm and install latest production node
+source $HOME/.nvm/nvm.sh
+nvm install v0.10.26
+nvm use v0.10.26
+
+# Install jshint to allow checking of JS code within emacs
+# http://jshint.com/
+npm install -g jshint
+
+# Install rlwrap to provide libreadline features with node
+# See: http://nodejs.org/api/repl.html#repl_repl
+sudo apt-get install -y rlwrap
+
+# Install emacs24
+# https://launchpad.net/~cassou/+archive/emacs
+sudo add-apt-repository -y ppa:cassou/emacs
+sudo apt-get -qq update
+sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
+
+# Install Prelude for emacs24
+# http://batsov.com/prelude/      
+curl -L http://git.io/epre | sh
+
+# Install Heroku toolbelt
+# https://toolbelt.heroku.com/debian
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+# Install MongoDB
+# The `apt-key` call registers the public key of the custom 10gen MongoDB aptitude repository
+# A custom 10gen repository list file is created containing the location of the MongoDB binaries
+# install MongoDB
+# https://www.digitalocean.com/community/articles/how-to-install-mongodb-on-ubuntu-12-04
+# http://docs.mongodb.org/manual/tutorial/install-mongodb-on-ubuntu/
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" | sudo tee -a /etc/apt/sources.list.d/10gen.list
+sudo apt-get -y update
+sudo apt-get -y install mongodb-10gen
+
+# Install MySQL
+# https://www.digitalocean.com/community/articles/how-to-install-linux-apache-mysql-php-lamp-stack-on-ubuntu
+sudo apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql
+sudo mysql_install_db
+sudo /usr/bin/mysql_secure_installation
+
+
+
+
+
+# git pull and install dotfiles as well
+cd $HOME
+if [ -d ./dotfiles/ ]; then
+    mv dotfiles dotfiles.old
+fi
+if [ -d .emacs.d/ ]; then
+    mv .emacs.d .emacs.d~
+fi
+git clone https://github.com/startup-class/dotfiles.git
+ln -sb dotfiles/.screenrc .
+ln -sb dotfiles/.bash_profile .
+ln -sb dotfiles/.bashrc .
+ln -sb dotfiles/.bashrc_custom .
+ln -sf dotfiles/.emacs.d .
